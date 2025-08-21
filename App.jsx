1a970b1f82ae10d05cc58fe505a11e6b0f50fb97
lolodev <horaciodev1706@gmail.com>
2025-08-21 20:19:37 -0300

    Create a Die component that takes a `value` prop. Should  *   render a button with that value displayed.  * - Render 10 instances of the Die component (manually)  *      - Provide a number between 1-6 for the value on each  *        for now  * - Style the <main> and <Die> components  *   to look like they do in the slide  *      - Hints: Create a container to hold the 10 instances  *        of the Die component, and use CSS Grid to lay them  *        out evenly in 2 rows of 5 columns  *      - Use flexbox on main to center the dice container  *        in the center of the page

--- App.jsx
+++ App.jsx
@@ -1,10 +1,18 @@
-import "./index.css";
+import Die from "./Die";
 
 export default function App() {
-  return (
-    <main>
-      <div className="container">
-      </div>
-    </main>
-  );
+  return <main>
+    <div className="dice-container">
+      <Die value={1} />
+      <Die value={2} />
+      <Die value={3} />
+      <Die value={4} />
+      <Die value={5} />
+      <Die value={5} />
+      <Die value={4} />
+      <Die value={3} />
+      <Die value={6} />
+      <Die value={2} />
+    </div>
+  </main>;
 }
