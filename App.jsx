99f92892436d21e12baef634cfcb1f5531726435
lolodev <horaciodev1706@gmail.com>
2025-08-22 16:44:52 -0300

    Scrimba way

--- App.jsx
+++ App.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
 
   function generateAllNewDice() {
-    return Array.from({ length: 10 }, () => ({
-      value: Math.ceil(Math.random() * 6),
-      isHeld: false
-    }));
+    return new Array(10)
+      .fill(0)
+      .map(() => ({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      }));
   }
 
-  const diceElements = dice.map(die => <Die value={die.value} isHeld={die.isHeld} />);
-
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} />);
+  console.log(diceElements);
   function rollDice() {
     setDice(generateAllNewDice());
   }
