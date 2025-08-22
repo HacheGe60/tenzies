29e0435d439def97aa157becfce3017ec8bca637
lolodev <horaciodev1706@gmail.com>
2025-08-21 22:32:55 -0300

    Scrimba way

--- App.jsx
+++ App.jsx
@@ -1,30 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import Die from "./Die";
 
-
-
-
 export default function App() {
-  const [dice, setDice] = React.useState(generateAllNewDice());
+  const [dice, setDice] = useState(generateAllNewDice());
 
   function generateAllNewDice() {
-    const newDice = [];
-    for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
-    }
-    return newDice;
-
-    /* return new Array(10)
-            .fill(0)
-            .map(() => Math.ceil(Math.random() * 6)) */
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6));
   }
 
+  const diceElements = dice.map(num => <Die value={num} />);
 
-  return <main>
-    <div className="dice-container">
-      {dice.map((die) => (
-        <Die value={die} />
-      ))}
-    </div>
-  </main>;
+  return (
+    <main>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+    </main>
+  );
 }
