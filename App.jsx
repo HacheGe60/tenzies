90c296af9aaf7b2020a0cad2fedb0f4f9a0fd462
lolodev <horaciodev1706@gmail.com>
2025-08-22 18:13:04 -0300

    Change btn text when game is over

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import { nanoid } from "nanoid";
 
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
-
+  const gameWon = dice.every(die => die.isHeld) && dice[0].value === dice[0].value;
 
   function generateAllNewDice() {
     return new Array(10)
@@ -24,22 +24,30 @@ export default function App() {
   />);
 
   function rollDice() {
-    setDice(generateAllNewDice());
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ?
+        die
+        : { ...die, value: Math.ceil(Math.random() * 6) };
+    }));
+
   }
 
   function hold(id) {
     setDice(prevDice => prevDice.map(die => {
-      return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      return die.id === id ?
+        { ...die, isHeld: !die.isHeld }
+        : die;
     }));
   }
 
 
   return (
-    <main>
+    <main><h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>{gameWon ? "New Game" : "Roll"}</button>
     </main>
   );
 }
