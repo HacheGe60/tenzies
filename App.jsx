1d800e5fe01b3487e4aa5a96a3fad8fbcc245f39
lolodev <horaciodev1706@gmail.com>
2025-08-21 22:23:35 -0300

    Write a function (allNewDice) that returns an array  of 10 random numbers between 1-6 inclusive.  Log the array of numbers to the console for now

--- App.jsx
+++ App.jsx
@@ -1,6 +1,18 @@
 import Die from "./Die";
 
 export default function App() {
+  function generateAllNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+
+    /* return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6)) */
+  }
+  console.log(generateAllNewDice());
   return <main>
     <div className="dice-container">
       <Die value={1} />
@@ -8,11 +20,11 @@ export default function App() {
       <Die value={3} />
       <Die value={4} />
       <Die value={5} />
-      <Die value={5} />
-      <Die value={4} />
-      <Die value={3} />
       <Die value={6} />
-      <Die value={2} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
     </div>
   </main>;
 }
